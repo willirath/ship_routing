becf8613bde1fe31068016a3cd24c92e5aeb0c59
Willi Rath <wrath@geomar.de>
2024-04-11 16:48:24 +0200

    Black

--- tests/test_cost.py
+++ tests/test_cost.py
@@ -3,6 +3,8 @@ from ship_routing.cost import power_maintain_speed
 
 
 import numpy as np
+import xarray as xr
+import pandas as pd
 
 import pytest
 
@@ -25,7 +27,7 @@ def test_cost_power_law():
 
     power_1 = power_maintain_speed(uo=uo, vo=vo, us=us, vs=vs)
     power_2 = power_maintain_speed(uo=2 * uo, vo=2 * vo, us=2 * us, vs=2 * vs)
-    np.testing.assert_almost_equal(2 ** 3, power_2 / power_1)
+    np.testing.assert_almost_equal(2**3, power_2 / power_1)
 
 
 def test_cost_coeff_dependency():
@@ -40,11 +42,18 @@ def test_cost_coeff_dependency():
     np.testing.assert_almost_equal(3.0, power_2 / power_1)
 
 
-@pytest.mark.parametrize("uuvv", [
-    [1, 2, 3, 4],
-    [1.0, 2.0, 3.0, 4.0],
-    [np.ones(shape=(123, )).copy() for n in range(4)]
-])
-def test_cost_dtypes(uuvv: list[int] | list[float] | list[NDArray[np.float64]]):
-    uo, vo, us, vs = uuvv
+@pytest.mark.parametrize(
+    "uovousvs",
+    [
+        [1, 2, 3, 4],
+        [1.0, 2.0, 3.0, 4.0],
+        [np.ones(shape=(123,)).copy() for n in range(4)],
+        [1.0, 1.0] + [np.ones(shape=(123,)).copy() for n in range(2)],
+        [xr.DataArray(np.ones(shape=(123,))) for n in range(4)],
+        [pd.Series(data=[1, 2, 3, 4]) for n in range(4)],
+    ],
+)
+def test_cost_dtypes(uovousvs):
+    """Test for many different data types."""
+    uo, vo, us, vs = uovousvs
     power_maintain_speed(uo=uo, vo=vo, us=us, vs=vs, coeff=1.0)
