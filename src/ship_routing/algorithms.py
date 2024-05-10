31931c49b682eada26539f64f9d6cfabdc069f8b
Willi Rath <wrath@geomar.de>
2024-05-10 14:03:05 +0200

    Add time gradient descent

--- src/ship_routing/algorithms.py
+++ src/ship_routing/algorithms.py
@@ -21,11 +21,13 @@ def gradient_descent_time_shift(
         ]
     )
     # TODO: Think about the normalization here...
-    gradients_norm = (gradients**2).sum() ** 0.5
     cost_before = route.cost_through(current_data_set=current_data_set)
     desired_cost_reduction = learning_rate_percent / 100 * cost_before
-    time_shift_sum = desired_cost_reduction / gradients_norm
-    time_shifts = gradients_norm / gradients * time_shift_sum / (len(route) - 2)
+    gradients_squared_sum = (gradients**2).sum()
+    if gradients_squared_sum != 0:
+        time_shifts = -desired_cost_reduction * gradients / gradients_squared_sum
+    else:
+        time_shifts = np.zeros_like(gradients)
     for n in range(1, len(route) - 1):
         ts = time_shifts[n - 1]
         if np.isinf(ts):
@@ -36,7 +38,4 @@ def gradient_descent_time_shift(
                 time_diff=ts * 1000 * np.timedelta64(1, "ms")
             ),
         )
-    print(gradients, gradients_norm)
-    print(cost_before, desired_cost_reduction)
-    print(time_shift_sum, time_shifts)
     return route
