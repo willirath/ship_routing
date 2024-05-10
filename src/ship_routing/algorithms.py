03c0382809ae87db656ca67fd669e4902f9abec9
Willi Rath <wrath@geomar.de>
2024-05-10 14:31:45 +0200

    Implement cross and along track gradient descent

--- src/ship_routing/algorithms.py
+++ src/ship_routing/algorithms.py
@@ -56,3 +56,111 @@ def gradient_descent_time_shift(
             ),
         )
     return route
+
+
+def gradient_descent_along_track(
+    route: Route = None,
+    current_data_set: xr.Dataset = None,
+    distance_meters: float = None,
+    learning_rate_percent: float = None,
+):
+    """Do one step of gradient descent with along-track shifts.
+
+    Parameters
+    ----------
+    route: Route
+        Initial route.
+    current_data_set: xr.Dataset
+        Contains currents.
+    distance_meters: float
+        Spatial shift used for estimating gradients.
+    learning_rate_percent: float
+        Desired learning rate in percent.
+
+    Returns
+    -------
+    Route:
+        Route with updated locations but unchanged times of way points.
+    """
+    gradients = np.array(
+        [
+            route.cost_gradient_along_track(
+                n=n,
+                current_data_set=current_data_set,
+                distance_meters=distance_meters,
+            )
+            for n in range(1, len(route) - 1)
+        ]
+    )
+    cost_before = route.cost_through(current_data_set=current_data_set)
+    desired_cost_reduction = learning_rate_percent / 100 * cost_before
+    gradients_squared_sum = (gradients**2).sum()
+    if gradients_squared_sum != 0:
+        dist_shifts = -desired_cost_reduction * gradients / gradients_squared_sum
+    else:
+        dist_shifts = np.zeros_like(gradients)
+    _route = route
+    for n in range(1, len(route) - 1):
+        ds = dist_shifts[n - 1]
+        if np.isinf(ds):
+            continue
+        _route = _route.move_waypoint(
+            n=n,
+            distance_meters=ds,
+            azimuth_degrees=route.waypoint_azimuth(n=n),
+        )
+    return _route
+
+
+def gradient_descent_across_track_left(
+    route: Route = None,
+    current_data_set: xr.Dataset = None,
+    distance_meters: float = None,
+    learning_rate_percent: float = None,
+):
+    """Do one step of gradient descent with across-track shifts.
+
+    Parameters
+    ----------
+    route: Route
+        Initial route.
+    current_data_set: xr.Dataset
+        Contains currents.
+    distance_meters: float
+        Spatial shift used for estimating gradients.
+    learning_rate_percent: float
+        Desired learning rate in percent.
+
+    Returns
+    -------
+    Route:
+        Route with updated locations but unchanged times of way points.
+    """
+    gradients = np.array(
+        [
+            route.cost_gradient_across_track_left(
+                n=n,
+                current_data_set=current_data_set,
+                distance_meters=distance_meters,
+            )
+            for n in range(1, len(route) - 1)
+        ]
+    )
+    cost_before = route.cost_through(current_data_set=current_data_set)
+    desired_cost_reduction = learning_rate_percent / 100 * cost_before
+    gradients_squared_sum = (gradients**2).sum()
+    if gradients_squared_sum != 0:
+        dist_shifts = -desired_cost_reduction * gradients / gradients_squared_sum
+    else:
+        dist_shifts = np.zeros_like(gradients)
+    _route = route
+    for n in range(1, len(route) - 1):
+        ds = dist_shifts[n - 1]
+        if np.isinf(ds):
+            continue
+        _route = _route.move_waypoint(
+            n=n,
+            distance_meters=ds,
+            azimuth_degrees=route.waypoint_azimuth(n=n) - 90.0,
+        )
+    return _route
--- tests/test_algorihms.py
+++ tests/test_algorihms.py
@@ -1,4 +1,8 @@
-from ship_routing.algorithms import gradient_descent_time_shift
+from ship_routing.algorithms import (
+    gradient_descent_time_shift,
+    gradient_descent_along_track,
+    gradient_descent_across_track_left,
+)
 
 from ship_routing.core import Route, WayPoint
 from ship_routing.currents import load_currents
@@ -8,6 +12,166 @@ import numpy as np
 from pathlib import Path
 
 TEST_DATA_DIR = Path(__file__).parent.resolve() / "test_data"
+
+
+def test_gradient_descent_across_track_left_atopt():
+    route_0 = Route(
+        way_points=(
+            WayPoint(lon=0.0, lat=0.0, time=np.datetime64("2001-01-01")),
+            WayPoint(lon=5.0, lat=0.0, time=np.datetime64("2001-01-02")),
+            WayPoint(lon=10.0, lat=0.0, time=np.datetime64("2001-01-03")),
+        )
+    )
+    # load currents and make zero
+    currents = load_currents(
+        data_file=TEST_DATA_DIR
+        / "currents/cmems_mod_glo_phy-cur_anfc_0.083deg_P1D-m_2021-01_100W-020E_10N-65N.nc"
+    )
+    currents = (0.0 * currents).fillna(0.0)
+
+    # The route above is already optimal. Let's check it's not changed.
+    route_1 = gradient_descent_across_track_left(
+        route=route_0,
+        current_data_set=currents,
+        distance_meters=1_000.0,
+        learning_rate_percent=1.0,
+    )
+
+    # calc cost
+    cost_0 = route_0.cost_through(currents)
+    cost_1 = route_1.cost_through(currents)
+
+    # ensure no reduction and no shifts
+    np.testing.assert_almost_equal(cost_1, cost_0)
+    np.testing.assert_array_almost_equal(
+        route_0.data_frame.lon, route_1.data_frame.lon, decimal=2
+    )
+    np.testing.assert_array_almost_equal(
+        route_0.data_frame.lat, route_1.data_frame.lat, decimal=2
+    )
+    np.testing.assert_array_almost_equal(
+        0.0,
+        (route_0.data_frame.time - route_1.data_frame.time) / np.timedelta64(1, "s"),
+        decimal=2,
+    )
+
+
+def test_gradient_descent_across_track_left_nonopt():
+    route_0 = Route(
+        way_points=(
+            WayPoint(lon=0.0, lat=0.0, time=np.datetime64("2001-01-01")),
+            WayPoint(lon=4.0, lat=0.0, time=np.datetime64("2001-01-02")),
+            WayPoint(lon=10.0, lat=0.0, time=np.datetime64("2001-01-03")),
+            WayPoint(lon=20.0, lat=0.0, time=np.datetime64("2001-01-04")),
+        )
+    )
+    # load currents and make zero
+    currents = load_currents(
+        data_file=TEST_DATA_DIR
+        / "currents/cmems_mod_glo_phy-cur_anfc_0.083deg_P1D-m_2021-01_100W-020E_10N-65N.nc"
+    )
+    currents = (0.0 * currents).fillna(0.0)
+
+    # With zero currents, cost is solely determined by speed over ground.
+    # The route above has three legs with increasing speeds over ground.
+    # The more equal the speeds of the two legs are the lower the cost.
+    # So making the low legs faster will reduce cost.
+    #
+    # update route
+    route_1 = gradient_descent_across_track_left(
+        route=route_0,
+        current_data_set=currents,
+        distance_meters=1_000.0,
+        learning_rate_percent=1.0,
+    )
+
+    # calc cost
+    cost_0 = route_0.cost_through(currents)
+    cost_1 = route_1.cost_through(currents)
+
+    # ensure reduction
+    assert cost_1 < cost_0
+    np.testing.assert_almost_equal(-1.0, 100.0 * (cost_1 - cost_0) / cost_0, decimal=1)
+
+
+def test_gradient_descent_along_track_atopt():
+    route_0 = Route(
+        way_points=(
+            WayPoint(lon=0.0, lat=0.0, time=np.datetime64("2001-01-01")),
+            WayPoint(lon=5.0, lat=0.0, time=np.datetime64("2001-01-02")),
+            WayPoint(lon=10.0, lat=0.0, time=np.datetime64("2001-01-03")),
+        )
+    )
+    # load currents and make zero
+    currents = load_currents(
+        data_file=TEST_DATA_DIR
+        / "currents/cmems_mod_glo_phy-cur_anfc_0.083deg_P1D-m_2021-01_100W-020E_10N-65N.nc"
+    )
+    currents = (0.0 * currents).fillna(0.0)
+
+    # The route above is already optimal. Let's check it's not changed.
+    route_1 = gradient_descent_along_track(
+        route=route_0,
+        current_data_set=currents,
+        distance_meters=1_000.0,
+        learning_rate_percent=1.0,
+    )
+
+    # calc cost
+    cost_0 = route_0.cost_through(currents)
+    cost_1 = route_1.cost_through(currents)
+
+    # ensure no reduction and no shifts
+    np.testing.assert_almost_equal(cost_1, cost_0)
+    np.testing.assert_array_almost_equal(
+        route_0.data_frame.lon, route_1.data_frame.lon, decimal=2
+    )
+    np.testing.assert_array_almost_equal(
+        route_0.data_frame.lat, route_1.data_frame.lat, decimal=2
+    )
+    np.testing.assert_array_almost_equal(
+        0.0,
+        (route_0.data_frame.time - route_1.data_frame.time) / np.timedelta64(1, "s"),
+        decimal=2,
+    )
+
+
+def test_gradient_descent_along_track_nonopt():
+    route_0 = Route(
+        way_points=(
+            WayPoint(lon=0.0, lat=0.0, time=np.datetime64("2001-01-01")),
+            WayPoint(lon=4.0, lat=0.0, time=np.datetime64("2001-01-02")),
+            WayPoint(lon=10.0, lat=0.0, time=np.datetime64("2001-01-03")),
+            WayPoint(lon=20.0, lat=0.0, time=np.datetime64("2001-01-04")),
+        )
+    )
+    # load currents and make zero
+    currents = load_currents(
+        data_file=TEST_DATA_DIR
+        / "currents/cmems_mod_glo_phy-cur_anfc_0.083deg_P1D-m_2021-01_100W-020E_10N-65N.nc"
+    )
+    currents = (0.0 * currents).fillna(0.0)
+
+    # With zero currents, cost is solely determined by speed over ground.
+    # The route above has three legs with increasing speeds over ground.
+    # The more equal the speeds of the two legs are the lower the cost.
+    # So making the low legs faster will reduce cost.
+    #
+    # update route
+    route_1 = gradient_descent_along_track(
+        route=route_0,
+        current_data_set=currents,
+        distance_meters=1_000.0,
+        learning_rate_percent=1.0,
+    )
+
+    # calc cost
+    cost_0 = route_0.cost_through(currents)
+    cost_1 = route_1.cost_through(currents)
+
+    # ensure reduction
+    assert cost_1 < cost_0
+    np.testing.assert_almost_equal(-1.0, 100.0 * (cost_1 - cost_0) / cost_0, decimal=1)
 
 
 def test_gradient_descent_time_shift_atopt():
