c1d4f73013ec459f3da729dd210a0e38260eb32e
Willi Rath <willirath@users.noreply.github.com>
2024-05-14 15:23:45 +0200

    Handle excessive learning rate expectations and invalid routes in gradient estimation (#37)
    * Test for edge case too large learning rate for the rem. possible optimisation
    
    * Explicitly handle covergence and invalid gradients

--- src/ship_routing/algorithms.py
+++ src/ship_routing/algorithms.py
@@ -4,6 +4,24 @@ import xarray as xr
 import numpy as np
 
 
+class InvalidGradientError(RuntimeError):
+    """Raised if gradients are invalid, e.g., because modified route on land."""
+
+    pass
+
+
+class ZeroGradientsError(RuntimeError):
+    """Raised if gradients are zero, indicating that route is an extremum."""
+
+    pass
+
+
+class LargeIncrementError(RuntimeError):
+    """Raised if shifts adjusted for learning rate are larger than the test increment."""
+
+    pass
+
+
 def gradient_descent_time_shift(
     route: Route = None,
     current_data_set: xr.Dataset = None,
@@ -41,14 +59,15 @@ def gradient_descent_time_shift(
     cost_before = route.cost_through(current_data_set=current_data_set)
     desired_cost_reduction = learning_rate_percent / 100 * cost_before
     gradients_squared_sum = (gradients**2).sum()
-    if gradients_squared_sum != 0:
-        time_shifts = -desired_cost_reduction * gradients / gradients_squared_sum
-    else:
-        time_shifts = np.zeros_like(gradients)
+    time_shifts = -desired_cost_reduction * gradients / gradients_squared_sum
+    if gradients_squared_sum == 0:
+        raise ZeroGradientsError
+    # Note that there's no invalid gradients here, because time shifts won't
+    # make a formerly valid route touch land.
+    if np.any(abs(time_shifts) > time_shift_seconds):
+        raise LargeIncrementError
     for n in range(1, len(route) - 1):
         ts = time_shifts[n - 1]
-        if np.isinf(ts):
-            continue
         route = route.replace_waypoint(
             n=n,
             new_way_point=route.way_points[n].move_time(
@@ -95,15 +114,17 @@ def gradient_descent_along_track(
     cost_before = route.cost_through(current_data_set=current_data_set)
     desired_cost_reduction = learning_rate_percent / 100 * cost_before
     gradients_squared_sum = (gradients**2).sum()
-    if gradients_squared_sum != 0:
-        dist_shifts = -desired_cost_reduction * gradients / gradients_squared_sum
-    else:
-        dist_shifts = np.zeros_like(gradients)
+    dist_shifts = -desired_cost_reduction * gradients / gradients_squared_sum
+    if gradients_squared_sum == 0:
+        raise ZeroGradientsError
+    if np.isnan(gradients_squared_sum):
+        raise InvalidGradientError
+    if np.any(abs(dist_shifts) > distance_meters):
+        raise LargeIncrementError
+    # keep a copy of the original route, because we need the original way point azimuths
     _route = route
     for n in range(1, len(route) - 1):
         ds = dist_shifts[n - 1]
-        if np.isinf(ds):
-            continue
         _route = _route.move_waypoint(
             n=n,
             distance_meters=ds,
@@ -149,18 +170,19 @@ def gradient_descent_across_track_left(
     cost_before = route.cost_through(current_data_set=current_data_set)
     desired_cost_reduction = learning_rate_percent / 100 * cost_before
     gradients_squared_sum = (gradients**2).sum()
-    if gradients_squared_sum != 0:
-        dist_shifts = -desired_cost_reduction * gradients / gradients_squared_sum
-    else:
-        dist_shifts = np.zeros_like(gradients)
+    dist_shifts = -desired_cost_reduction * gradients / gradients_squared_sum
+    if gradients_squared_sum == 0:
+        raise ZeroGradientsError
+    if np.isnan(gradients_squared_sum):
+        raise InvalidGradientError
+    if np.any(abs(dist_shifts) > distance_meters):
+        raise LargeIncrementError
+    # keep a copy of the original route, because we need the original way point azimuths
     _route = route
     for n in range(1, len(route) - 1):
-        ds = dist_shifts[n - 1]
-        if np.isinf(ds):
-            continue
         _route = _route.move_waypoint(
             n=n,
-            distance_meters=ds,
+            distance_meters=dist_shifts[n - 1],
             azimuth_degrees=route.waypoint_azimuth(n=n) - 90.0,
         )
     return _route
--- tests/test_algorihms.py
+++ tests/test_algorihms.py
@@ -2,19 +2,23 @@ from ship_routing.algorithms import (
     gradient_descent_time_shift,
     gradient_descent_along_track,
     gradient_descent_across_track_left,
+    InvalidGradientError,
+    ZeroGradientsError,
+    LargeIncrementError,
 )
 
 from ship_routing.core import Route, WayPoint
 from ship_routing.currents import load_currents
 
 import numpy as np
+import pytest
 
 from pathlib import Path
 
 TEST_DATA_DIR = Path(__file__).parent.resolve() / "test_data"
 
 
-def test_gradient_descent_across_track_left_atopt():
+def test_gradient_descent_across_track_left_zero_gradients_error():
     route_0 = Route(
         way_points=(
             WayPoint(lon=0.0, lat=0.0, time=np.datetime64("2001-01-01")),
@@ -22,38 +26,194 @@ def test_gradient_descent_across_track_left_atopt():
             WayPoint(lon=10.0, lat=0.0, time=np.datetime64("2001-01-03")),
         )
     )
-    # load currents and make zero
-    currents = load_currents(
-        data_file=TEST_DATA_DIR
-        / "currents/cmems_mod_glo_phy-cur_anfc_0.083deg_P1D-m_2021-01_100W-020E_10N-65N.nc"
-    )
-    currents = (0.0 * currents).fillna(0.0)
+    zero_currents = (
+        0.0
+        * load_currents(
+            data_file=TEST_DATA_DIR
+            / "currents/cmems_mod_glo_phy-cur_anfc_0.083deg_P1D-m_2021-01_100W-020E_10N-65N.nc"
+        )
+    ).fillna(0.0)
+    with pytest.raises(ZeroGradientsError):
+        _ = gradient_descent_across_track_left(
+            route=route_0,
+            current_data_set=zero_currents,
+            distance_meters=2_000.0,
+            learning_rate_percent=0.1,
+        )
 
-    # The route above is already optimal. Let's check it's not changed.
-    route_1 = gradient_descent_across_track_left(
-        route=route_0,
-        current_data_set=currents,
-        distance_meters=1_000.0,
-        learning_rate_percent=1.0,
-    )
 
-    # calc cost
-    cost_0 = route_0.cost_through(currents)
-    cost_1 = route_1.cost_through(currents)
+def test_gradient_descent_along_track_zero_gradients_error():
+    route_0 = Route(
+        way_points=(
+            WayPoint(lon=0.0, lat=0.0, time=np.datetime64("2001-01-01")),
+            WayPoint(lon=5.0, lat=0.0, time=np.datetime64("2001-01-02")),
+            WayPoint(lon=10.0, lat=0.0, time=np.datetime64("2001-01-03")),
+        )
+    )
+    zero_currents = (
+        0.0
+        * load_currents(
+            data_file=TEST_DATA_DIR
+            / "currents/cmems_mod_glo_phy-cur_anfc_0.083deg_P1D-m_2021-01_100W-020E_10N-65N.nc"
+        )
+    ).fillna(0.0)
+    with pytest.raises(ZeroGradientsError):
+        _ = gradient_descent_along_track(
+            route=route_0,
+            current_data_set=zero_currents,
+            distance_meters=2_000.0,
+            learning_rate_percent=0.1,
+        )
 
-    # ensure no reduction and no shifts
-    np.testing.assert_almost_equal(cost_1, cost_0)
-    np.testing.assert_array_almost_equal(
-        route_0.data_frame.lon, route_1.data_frame.lon, decimal=2
+
+def test_gradient_descent_time_shift_zero_gradients_error():
+    route_0 = Route(
+        way_points=(
+            WayPoint(lon=0.0, lat=0.0, time=np.datetime64("2001-01-01")),
+            WayPoint(lon=5.0, lat=0.0, time=np.datetime64("2001-01-02")),
+            WayPoint(lon=10.0, lat=0.0, time=np.datetime64("2001-01-03")),
+        )
     )
-    np.testing.assert_array_almost_equal(
-        route_0.data_frame.lat, route_1.data_frame.lat, decimal=2
+    zero_currents = (
+        0.0
+        * load_currents(
+            data_file=TEST_DATA_DIR
+            / "currents/cmems_mod_glo_phy-cur_anfc_0.083deg_P1D-m_2021-01_100W-020E_10N-65N.nc"
+        )
+    ).fillna(0.0)
+    with pytest.raises(ZeroGradientsError):
+        _ = gradient_descent_time_shift(
+            route=route_0,
+            current_data_set=zero_currents,
+            time_shift_seconds=1_000.0,
+            learning_rate_percent=0.1,
+        )
+
+
+def test_gradient_descent_across_track_left_large_increment_error():
+    route_0 = Route(
+        way_points=(
+            WayPoint(lon=0.0, lat=0.0, time=np.datetime64("2001-01-01")),
+            WayPoint(lon=5.0, lat=1.0, time=np.datetime64("2001-01-02")),
+            WayPoint(lon=10.0, lat=0.0, time=np.datetime64("2001-01-03")),
+        )
     )
-    np.testing.assert_array_almost_equal(
-        0.0,
-        (route_0.data_frame.time - route_1.data_frame.time) / np.timedelta64(1, "s"),
-        decimal=2,
+    zero_currents = (
+        0.0
+        * load_currents(
+            data_file=TEST_DATA_DIR
+            / "currents/cmems_mod_glo_phy-cur_anfc_0.083deg_P1D-m_2021-01_100W-020E_10N-65N.nc"
+        )
+    ).fillna(0.0)
+    with pytest.raises(LargeIncrementError):
+        _ = gradient_descent_across_track_left(
+            route=route_0,
+            current_data_set=zero_currents,
+            distance_meters=2_000.0,
+            learning_rate_percent=10,
+        )
+
+
+def test_gradient_descent_along_track_large_increment_error():
+    route_0 = Route(
+        way_points=(
+            WayPoint(lon=0.0, lat=0.0, time=np.datetime64("2001-01-01")),
+            WayPoint(lon=6.0, lat=0.0, time=np.datetime64("2001-01-02")),
+            WayPoint(lon=10.0, lat=0.0, time=np.datetime64("2001-01-03")),
+        )
     )
+    zero_currents = (
+        0.0
+        * load_currents(
+            data_file=TEST_DATA_DIR
+            / "currents/cmems_mod_glo_phy-cur_anfc_0.083deg_P1D-m_2021-01_100W-020E_10N-65N.nc"
+        )
+    ).fillna(0.0)
+    with pytest.raises(LargeIncrementError):
+        _ = gradient_descent_along_track(
+            route=route_0,
+            current_data_set=zero_currents,
+            distance_meters=2_000.0,
+            learning_rate_percent=10,
+        )
+
+
+def test_gradient_descent_time_shift_large_increment_error():
+    route_0 = Route(
+        way_points=(
+            WayPoint(lon=0.0, lat=0.0, time=np.datetime64("2001-01-01")),
+            WayPoint(lon=5.0, lat=0.0, time=np.datetime64("2001-01-02T01:00:00")),
+            WayPoint(lon=10.0, lat=0.0, time=np.datetime64("2001-01-03")),
+        )
+    )
+    zero_currents = (
+        0.0
+        * load_currents(
+            data_file=TEST_DATA_DIR
+            / "currents/cmems_mod_glo_phy-cur_anfc_0.083deg_P1D-m_2021-01_100W-020E_10N-65N.nc"
+        )
+    ).fillna(0.0)
+    with pytest.raises(LargeIncrementError):
+        _ = gradient_descent_time_shift(
+            route=route_0,
+            current_data_set=zero_currents,
+            time_shift_seconds=100.0,
+            learning_rate_percent=10.0,
+        )
+
+
+def test_gradient_descent_across_track_left_invalid_gradient_error():
+    route_0 = Route(
+        way_points=(
+            WayPoint(lon=0.0, lat=0.0, time=np.datetime64("2001-01-01")),
+            WayPoint(lon=5.0, lat=1.0, time=np.datetime64("2001-01-02")),
+            WayPoint(lon=10.0, lat=0.0, time=np.datetime64("2001-01-03")),
+        )
+    )
+    zero_currents = (
+        0.0
+        * load_currents(
+            data_file=TEST_DATA_DIR
+            / "currents/cmems_mod_glo_phy-cur_anfc_0.083deg_P1D-m_2021-01_100W-020E_10N-65N.nc"
+        )
+    ).fillna(0.0)
+    zero_currents_with_invalid_region = zero_currents.where(zero_currents.lat <= 1.0)
+    with pytest.raises(InvalidGradientError):
+        _ = gradient_descent_across_track_left(
+            route=route_0,
+            current_data_set=zero_currents_with_invalid_region,
+            distance_meters=100_000.0,
+            learning_rate_percent=0.1,
+        )
+
+
+def test_gradient_descent_along_track_invalid_gradient_error():
+    route_0 = Route(
+        way_points=(
+            WayPoint(lon=0.0, lat=0.0, time=np.datetime64("2001-01-01")),
+            WayPoint(lon=5.0, lat=5.0, time=np.datetime64("2001-01-02")),
+            WayPoint(lon=10.0, lat=0.0, time=np.datetime64("2001-01-03")),
+        )
+    )
+    zero_currents = (
+        0.0
+        * load_currents(
+            data_file=TEST_DATA_DIR
+            / "currents/cmems_mod_glo_phy-cur_anfc_0.083deg_P1D-m_2021-01_100W-020E_10N-65N.nc"
+        )
+    ).fillna(0.0)
+    lon = zero_currents.lon
+    lat = zero_currents.lat
+    zero_currents_with_invalid_region = zero_currents.where(
+        ((lon - lat) > 0.01) & (((10 - lon) - lat) > 0.01) & (lat >= 0)
+    )
+    with pytest.raises(InvalidGradientError):
+        _ = gradient_descent_along_track(
+            route=route_0,
+            current_data_set=zero_currents_with_invalid_region,
+            distance_meters=100_000.0,
+            learning_rate_percent=0.1,
+        )
 
 
 def test_gradient_descent_across_track_left_nonopt():
@@ -81,8 +241,8 @@ def test_gradient_descent_across_track_left_nonopt():
     route_1 = gradient_descent_across_track_left(
         route=route_0,
         current_data_set=currents,
-        distance_meters=1_000.0,
-        learning_rate_percent=1.0,
+        distance_meters=2_000.0,
+        learning_rate_percent=0.1,
     )
 
     # calc cost
@@ -91,49 +251,7 @@ def test_gradient_descent_across_track_left_nonopt():
 
     # ensure reduction
     assert cost_1 < cost_0
-    np.testing.assert_almost_equal(-1.0, 100.0 * (cost_1 - cost_0) / cost_0, decimal=1)
-
-
-def test_gradient_descent_along_track_atopt():
-    route_0 = Route(
-        way_points=(
-            WayPoint(lon=0.0, lat=0.0, time=np.datetime64("2001-01-01")),
-            WayPoint(lon=5.0, lat=0.0, time=np.datetime64("2001-01-02")),
-            WayPoint(lon=10.0, lat=0.0, time=np.datetime64("2001-01-03")),
-        )
-    )
-    # load currents and make zero
-    currents = load_currents(
-        data_file=TEST_DATA_DIR
-        / "currents/cmems_mod_glo_phy-cur_anfc_0.083deg_P1D-m_2021-01_100W-020E_10N-65N.nc"
-    )
-    currents = (0.0 * currents).fillna(0.0)
-
-    # The route above is already optimal. Let's check it's not changed.
-    route_1 = gradient_descent_along_track(
-        route=route_0,
-        current_data_set=currents,
-        distance_meters=1_000.0,
-        learning_rate_percent=1.0,
-    )
-
-    # calc cost
-    cost_0 = route_0.cost_through(currents)
-    cost_1 = route_1.cost_through(currents)
-
-    # ensure no reduction and no shifts
-    np.testing.assert_almost_equal(cost_1, cost_0)
-    np.testing.assert_array_almost_equal(
-        route_0.data_frame.lon, route_1.data_frame.lon, decimal=2
-    )
-    np.testing.assert_array_almost_equal(
-        route_0.data_frame.lat, route_1.data_frame.lat, decimal=2
-    )
-    np.testing.assert_array_almost_equal(
-        0.0,
-        (route_0.data_frame.time - route_1.data_frame.time) / np.timedelta64(1, "s"),
-        decimal=2,
-    )
+    np.testing.assert_almost_equal(-0.1, 100.0 * (cost_1 - cost_0) / cost_0, decimal=1)
 
 
 def test_gradient_descent_along_track_nonopt():
@@ -161,8 +279,8 @@ def test_gradient_descent_along_track_nonopt():
     route_1 = gradient_descent_along_track(
         route=route_0,
         current_data_set=currents,
-        distance_meters=1_000.0,
-        learning_rate_percent=1.0,
+        distance_meters=2_000.0,
+        learning_rate_percent=0.1,
     )
 
     # calc cost
@@ -171,49 +289,7 @@ def test_gradient_descent_along_track_nonopt():
 
     # ensure reduction
     assert cost_1 < cost_0
-    np.testing.assert_almost_equal(-1.0, 100.0 * (cost_1 - cost_0) / cost_0, decimal=1)
-
-
-def test_gradient_descent_time_shift_atopt():
-    route_0 = Route(
-        way_points=(
-            WayPoint(lon=0.0, lat=0.0, time=np.datetime64("2001-01-01")),
-            WayPoint(lon=5.0, lat=0.0, time=np.datetime64("2001-01-02")),
-            WayPoint(lon=10.0, lat=0.0, time=np.datetime64("2001-01-03")),
-        )
-    )
-    # load currents and make zero
-    currents = load_currents(
-        data_file=TEST_DATA_DIR
-        / "currents/cmems_mod_glo_phy-cur_anfc_0.083deg_P1D-m_2021-01_100W-020E_10N-65N.nc"
-    )
-    currents = (0.0 * currents).fillna(0.0)
-
-    # The route above is already optimal. Let's check it's not changed.
-    route_1 = gradient_descent_time_shift(
-        route=route_0,
-        current_data_set=currents,
-        time_shift_seconds=1200.0,
-        learning_rate_percent=1.0,
-    )
-
-    # calc cost
-    cost_0 = route_0.cost_through(currents)
-    cost_1 = route_1.cost_through(currents)
-
-    # ensure reduction
-    np.testing.assert_almost_equal(cost_1, cost_0)
-    np.testing.assert_array_almost_equal(
-        route_0.data_frame.lon, route_1.data_frame.lon, decimal=2
-    )
-    np.testing.assert_array_almost_equal(
-        route_0.data_frame.lat, route_1.data_frame.lat, decimal=2
-    )
-    np.testing.assert_array_almost_equal(
-        0.0,
-        (route_0.data_frame.time - route_1.data_frame.time) / np.timedelta64(1, "s"),
-        decimal=2,
-    )
+    np.testing.assert_almost_equal(-0.1, 100.0 * (cost_1 - cost_0) / cost_0, decimal=1)
 
 
 def test_gradient_descent_time_shift_nonopt():
@@ -242,7 +318,7 @@ def test_gradient_descent_time_shift_nonopt():
         route=route_0,
         current_data_set=currents,
         time_shift_seconds=1200.0,
-        learning_rate_percent=1.0,
+        learning_rate_percent=0.1,
     )
 
     # calc cost
@@ -251,4 +327,4 @@ def test_gradient_descent_time_shift_nonopt():
 
     # ensure reduction
     assert cost_1 < cost_0
-    np.testing.assert_almost_equal(-1.0, 100.0 * (cost_1 - cost_0) / cost_0, decimal=1)
+    np.testing.assert_almost_equal(-0.1, 100.0 * (cost_1 - cost_0) / cost_0, decimal=1)
