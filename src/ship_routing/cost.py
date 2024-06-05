b1d16564dfc31857accc62c97487edafc3403c70
Willi Rath <wrath@geomar.de>
2024-06-05 17:06:22 +0200

    Implement xr power calc

--- src/ship_routing/cost.py
+++ src/ship_routing/cost.py
@@ -1,151 +1,81 @@
 from .config import Ship, Physics, SHIP_DEFAULT, PHYSICS_DEFAULT
+from .cost_ufuncs import power_maintain_speed_ufunc, hazard_conditions_wave_height_ufunc
+
+
+import numpy as np
+import xarray as xr
+
+
+def align_along_track_arrays(*argv) -> tuple:
+    """Align all fields on their `along` dimension."""
+    # find longest array
+    along_sizes = [a.sizes["along"] for a in argv]
+    i_of_longest = np.argmax(along_sizes)
+    a_ref = argv[i_of_longest]
+
+    # interpolate shorter ones on the longer
+    return tuple((a.interp(along=a_ref.along, method="nearest") for a in argv))
+
+
+def maybe_cast_number_to_data_array(obj):
+    """Make obj a data array with one along-track point."""
+    if np.array(obj).shape == ():
+        obj = xr.DataArray(
+            [obj],
+            dims=("along",),
+            coords={"along": [0]},
+        )
+    return obj
 
 
 def power_maintain_speed(
-    u_ship_og_ms: float = None,
-    v_ship_og_ms: float = None,
-    u_current_ms: float = 0.0,
-    v_current_ms: float = 0.0,
-    u_wind_ms: float = 0.0,
-    v_wind_ms: float = 0.0,
-    w_wave_height: float = 0.0,
+    u_ship_og_ms: xr.DataArray = 0.0,
+    v_ship_og_ms: xr.DataArray = 0.0,
+    u_current_ms: xr.DataArray = 0.0,
+    v_current_ms: xr.DataArray = 0.0,
+    u_wind_ms: xr.DataArray = 0.0,
+    v_wind_ms: xr.DataArray = 0.0,
+    w_wave_height: xr.DataArray = 0.0,
     physics: Physics = PHYSICS_DEFAULT,
     ship: Ship = SHIP_DEFAULT,
-) -> float:
-    """Calculate power needed to maintain speed over ground.
+):
+    # cast all to arrays
+    u_ship_og_ms = maybe_cast_number_to_data_array(u_ship_og_ms)
+    v_ship_og_ms = maybe_cast_number_to_data_array(v_ship_og_ms)
+    u_current_ms = maybe_cast_number_to_data_array(u_current_ms)
+    v_current_ms = maybe_cast_number_to_data_array(v_current_ms)
+    u_wind_ms = maybe_cast_number_to_data_array(u_wind_ms)
+    v_wind_ms = maybe_cast_number_to_data_array(v_wind_ms)
+    w_wave_height = maybe_cast_number_to_data_array(w_wave_height)
 
-    This largely implements the resistance estimates due to calm water and sea
-    state as outlined in
-        Mannarini, G., Pinardi, N., Coppini, G., Oddo, P., and Iafrati, A.:
-        VISIR-I: small vessels – least-time nautical routes using wave forecasts,
-        Geosci. Model Dev., 9, 1597–1625, 2016. https://doi.org/10.5194/gmd-9-1597-2016
-    and wind resistance as outlined in
-        Kim K-S, Roh M-I. ISO 15016:2015-Based Method for Estimating the Fuel Oil Consumption
-        of a Ship. Journal of Marine Science and Engineering. 2020; 8(10):791.
-        https://doi.org/10.3390/jmse8100791
-
-    Note that for zero wave height, there is no additional resistance. For zero winds and
-    currents, however, resistance from relative movement in, respectively, water and air
-    will be accounted for. Hence, to turn off wind resistance, we need to set either the projected
-    frontal area or the wind resistance coefficient to zero.
-
-    Parameters
-    ----------
-    u_ship_og: array
-        Ship eastward speed over ground in meters per second.
-    v_ship_og: array
-        Ship northward speed over ground in meters per second.
-        Needs same shape as u_ship_og.
-    u_current: array
-        Ocean currents eastward speed over ground in meters per second.
-        Needs shape that can be broadcast to shape of u_ship_og and v_ship_og.
-    v_current: array
-        Ocean currents northward speed over ground in meters per second.
-        Needs shape that can be broadcast to shape of u_ship_og and v_ship_og.
-    u_wind: array
-        Eastward 10 m wind in m/s
-        Needs shape that can be broadcst to shape of u_ship_og and v_ship_og
-    v_wind: array
-        Northward 10 m wind in m/s
-        Needs shape that can be broadcst to shape of u_ship_og and v_ship_og
-    w_wave_height: array
-        Spectral significant wave height (Hm0), meters
-        Needs shape that can be broadcst to shape of u_ship_og and v_ship_og
-    physics: Physics
-        Physics parameters.
-    ship: Ship
-        Ship parameters.
-
-    -------
-    array:
-        Power in W (=kg*m2/s3) needed to maintain speed over ground for each
-        element of u_ship_og and v_ship_og. Shape will be identical to
-        u_ship_og and v_ship_og.
-    """
-
-    # speed through water
-    speed_through_water_ms = (
-        (u_ship_og_ms - u_current_ms) ** 2 + (v_ship_og_ms - v_current_ms) ** 2
-    ) ** 0.5
-    speed_through_wind_ms = (
-        (u_ship_og_ms - u_wind_ms) ** 2 + (v_ship_og_ms - v_wind_ms) ** 2
-    ) ** 0.5
-
-    # resistance through calm water (Note that calm only refers to the sea state
-    # and does not imply absence of currents.)
-    reference_resistance_calm = (
-        ship.total_propulsive_efficiency
-        * ship.reference_engine_power_W
-        / ship.reference_speed_calm_water_ms**3
+    # align all
+    (
+        u_ship_og_ms,
+        v_ship_og_ms,
+        u_current_ms,
+        v_current_ms,
+        u_wind_ms,
+        v_wind_ms,
+        w_wave_height,
+    ) = align_along_track_arrays(
+        u_ship_og_ms,
+        v_ship_og_ms,
+        u_current_ms,
+        v_current_ms,
+        u_wind_ms,
+        v_wind_ms,
+        w_wave_height,
     )
-    power_through_water = reference_resistance_calm * speed_through_water_ms**3
 
-    # resistance through sea waves
-    spectral_average = 0.5
-    nondimensional_resistance_reference = (
-        20.0
-        * (ship.waterline_width_m / ship.waterline_length_m) ** (-1.20)
-        * (ship.draught_m / ship.waterline_length_m) ** 0.62
+    # calc power
+    return power_maintain_speed_ufunc(
+        u_ship_og_ms=u_ship_og_ms,
+        v_ship_og_ms=v_ship_og_ms,
+        u_current_ms=u_current_ms,
+        v_current_ms=v_current_ms,
+        u_wind_ms=u_wind_ms,
+        v_wind_ms=v_wind_ms,
+        w_wave_height=w_wave_height,
+        physics=physics,
+        ship=ship,
     )
-    froude_number_reference = (
-        ship.reference_speed_calm_water_ms
-        / (physics.gravity_acceleration_ms2 * ship.waterline_length_m) ** 0.5
-    )
-    froude_number = (
-        speed_through_water_ms
-        / (physics.gravity_acceleration_ms2 * ship.waterline_length_m) ** 0.5
-    )
-    nondimensional_resistance = (
-        nondimensional_resistance_reference / froude_number_reference * froude_number
-    )
-    resistance_through_sea_waves = (
-        nondimensional_resistance
-        * physics.sea_water_density_kgm3
-        * physics.gravity_acceleration_ms2
-        * (w_wave_height / 2.0) ** 2  # wave amplitude = height / 2
-        * ship.waterline_width_m**2
-        / ship.waterline_length_m
-        * spectral_average
-    )
-    power_through_waves = resistance_through_sea_waves * speed_through_water_ms
-
-    # resistance through wind. (Note simplify this to the case where the apparent
-    # wind comes from the front.)
-    resistance_through_wind = (
-        0.5
-        * ship.wind_resistance_coefficient
-        * ship.projected_frontal_area_above_waterline_m2
-        * physics.air_density_kgm3
-        * speed_through_wind_ms**2
-    )
-    power_through_wind = resistance_through_wind * speed_through_wind_ms
-
-    return power_through_water + power_through_waves + power_through_wind
-
-
-def hazard_conditions_wave_height(
-    ship: Ship = Ship(),
-    w_wave_height_m: float = 0,
-) -> bool:
-    """Check stability thresholds for wave heights.
-
-    Follows
-        Mannarini, G., Pinardi, N., Coppini, G., Oddo, P., and Iafrati, A.:
-        VISIR-I: small vessels – least-time nautical routes using wave forecasts,
-        Geosci. Model Dev., 9, 1597–1625, 2016. https://doi.org/10.5194/gmd-9-1597-2016
-    but only accounts for ration of wave height and ship length. Disregarding wave period
-    and angle of attack, this amounts to cheching wether wave height / ship length < 1/40.
-
-    Parameters
-    ----------
-    ship: Ship
-        Ship parameters.
-    w_wave_height_m: float
-       Significant wave height in meters. Defaults to: 0.0
-
-    Returns
-    -------
-    bool:
-        Whether any of the stability thresholds is violated.
-    """
-    return (w_wave_height_m / ship.waterline_length_m) < 1 / 40.0
---
+++ tests/test_cost.py
@@ -0,0 +1,38 @@
+from ship_routing.cost import (
+    align_along_track_arrays,
+    maybe_cast_number_to_data_array,
+    power_maintain_speed,
+)
+
+
+import xarray as xr
+import numpy as np
+
+
+def test_align_along_track_arrrays():
+    a0 = xr.DataArray(
+        list(range(7)),
+        dims=("along",),
+        coords={"along": np.linspace(0, 1, 7)},
+    )
+    a1 = xr.DataArray(
+        list(range(1)),
+        dims=("along",),
+        coords={"along": np.linspace(0, 1, 1)},
+    )
+    a2 = xr.DataArray(
+        list(range(17)),
+        dims=("along",),
+        coords={"along": np.linspace(0, 1, 17)},
+    )
+    a0a, a1a, a2a = align_along_track_arrays(a0, a1, a2)
+    assert a0a.sizes["along"] == a1a.sizes["along"]
+    assert a0a.sizes["along"] == a2a.sizes["along"]
+
+
+def test_maybe_cast_number_to_data_array():
+    assert "along" in maybe_cast_number_to_data_array(1).dims
+
+
+def test_power_maintain_speed_just_call():
+    power_maintain_speed()
