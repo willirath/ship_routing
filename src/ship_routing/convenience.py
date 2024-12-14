be09efb40de7368e84eda1c1ade3e865bb92e6ba
Willi Rath <wrath@geomar.de>
2024-12-14 13:14:51 +0100

    Add logs

--- src/ship_routing/convenience.py
+++ src/ship_routing/convenience.py
@@ -1,8 +1,38 @@
 from .core import Route, WayPoint
 
 import numpy as np
+import pandas as pd
 import xarray as xr
 import tqdm
+
+
+from dataclasses import dataclass, asdict
+
+
+@dataclass(frozen=True)
+class Logs:
+    iteration: int = 0
+    cost: float = 0.0
+    method: str = "initial"
+
+    @property
+    def data_frame(self):
+        return pd.DataFrame(
+            asdict(self),
+            index=[
+                0,
+            ],
+        )
+
+
+@dataclass(frozen=True)
+class LogsRoute:
+    logs: Logs
+    route: Route
+
+    @property
+    def data_frame(self):
+        return self.logs.data_frame.join(self.route.data_frame, how="cross")
 
 
 def create_route(
@@ -38,27 +68,32 @@ def stochastic_search(
     refinement_factor: float = 0.5,
     mod_width: float = None,
     max_move_meters: float = None,
-    include_logging: bool = True,
+    include_logs_routes: bool = True,
     current_data_set: xr.Dataset = None,
     wave_data_set: xr.Dataset = None,
     wind_data_set: xr.Dataset = None,
 ) -> Route:
-    if include_logging:
-        cost_steps = []
+    if include_logs_routes:
+        logs_routes = []
     else:
-        cost_steps = None
+        logs_routes = None
 
     cost = route.cost_through(
         current_data_set=current_data_set,
         wave_data_set=wave_data_set,
         wind_data_set=wind_data_set,
     )
-    if include_logging:
-        cost_steps.append(cost)
+    if include_logs_routes:
+        logs_routes.append(
+            LogsRoute(
+                logs=Logs(iteration=-1, cost=cost, method="initial"),
+                route=route,
+            )
+        )
 
     accepted = 0
     n_reset = 0
-    for n in tqdm.tqdm(range(number_of_iterations)):
+    for iteration in tqdm.tqdm(range(number_of_iterations)):
         route_ = route.move_waypoints_left_nonlocal(
             center_distance_meters=np.random.uniform(
                 mod_width / 2.0, route.length_meters - mod_width / 2.0
@@ -78,8 +113,15 @@ def stochastic_search(
             route = route_
             cost = cost_
             accepted += 1
-            if include_logging:
-                cost_steps.append(cost)
+            if include_logs_routes:
+                logs_routes.append(
+                    LogsRoute(
+                        logs=Logs(
+                            iteration=iteration, cost=cost, method="stochastic_search"
+                        ),
+                        route=route,
+                    )
+                )
         if (accepted + 1) / (n_reset + 1) < acceptance_rate_target:
             n_reset = 0
             accepted = 0
@@ -88,4 +130,4 @@ def stochastic_search(
 
         n_reset += 1
 
-    return route, cost_steps
+    return route, logs_routes
